c79d566b0021f24500900604bc8ad5d109aeb20d
wayrayfr <getsetgo1002@gmail.com>
2025-06-10 12:43:56 +0900

    Update train.py

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader
 from utils import misc
 from utils import arg_util
 from utils.data import build_dataset
-from utils.data_sampler import DistInfiniteBatchSampler, EvalDistributedSampler
+from utils.data_sampler import InfiniteBatchSampler
 from utils.misc import auto_resume
 
 import importlib
